21ea31a546b810e5b41e8a1883e2aaeb3f742276
Jayasree Lekkalapudi <jayasreel@gtmbuddy.ai>
2023-06-27 22:12:35 +0530

    initial commit for spring boot reddit

---
+++ src/test/java/com/oreilly/reddit/RedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oreilly.reddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
